d7f5147e27373f78efab4d694e1b82bc42092dee
Veniel Dorestave <veniel@noukod.com>
2018-10-04 15:06:35 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,7 +12,7 @@ class Song
   def self.new_by_filename(filename)
    song = self.new filename.split(" - ")[1])
     song.artist_name = (filename.split(" - ")[0])
-    song
+    # song
   end
 
   def self.find_by_artist(artist)
