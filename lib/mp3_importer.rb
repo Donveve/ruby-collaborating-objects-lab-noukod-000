b125f84c38417c7ccc55369a8e9c07b035c0f982
Veniel Dorestave <veniel@noukod.com>
2018-10-04 15:13:35 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -9,11 +9,11 @@ class MP3Importer
     @file = Dir.entries(path).select{|file| file[/\.mp3$/]}
     end
 
-  
+
 
   def import
     self.files.each do |filename|
       Song.new_by_filename(filename)
     end
   end
-end
+
