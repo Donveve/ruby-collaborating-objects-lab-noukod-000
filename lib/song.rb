465ffef7e9c760d9f2434f45c4f3cb2e8958dcbe
Veniel Dorestave <veniel@noukod.com>
2018-10-04 15:08:58 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,7 +3,7 @@
 class Song
   attr_accessor :name, :artist
 
-  @@all = []
+  # @@all = []
 
   def initialize(name)
     @name = name
@@ -12,26 +12,26 @@ class Song
   def self.new_by_filename(filename)
    song = self.new filename.split(" - ")[1])
     song.artist_name = (filename.split(" - ")[0])
-    # song
+     song
   end
 
-  def self.find_by_artist(artist)
-    Song.all.select do | song |
-      song.artist == artist
-    end
-  end
+  # def self.find_by_artist(artist)
+  #   Song.all.select do | song |
+  #     song.artist == artist
+  #   end
+  # end
 
-  def self.all
-    @@all
-  end
+  # def self.all
+  #   @@all
+  # end
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by_name(name)
     artist.add_song(self)
   end
 
-  def save
-    @@all << self
-    self
-  end
+  # def save
+  #   @@all << self
+  #   self
+  # end
 end
