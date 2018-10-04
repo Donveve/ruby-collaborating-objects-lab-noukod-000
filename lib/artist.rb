bbd1098a3464b3a797dec3d8c25b71436e033a3c
Veniel Dorestave <veniel@noukod.com>
2018-10-04 13:26:24 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -31,10 +31,10 @@ class Artist
     @@all << self
   end
 
-  def print_songs
-  		@songs.each do |song|
-  			puts song.name
-  		end
-  	end
+  # def print_songs
+  # 		@songs.each do |song|
+  # 			puts song.name
+  # 		end
+  # 	end
 
 end
