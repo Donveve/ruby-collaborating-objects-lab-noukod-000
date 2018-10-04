e475b2083e7c655b0c5658c1e6a20aea0c63acc6
Veniel Dorestave <veniel@noukod.com>
2018-10-04 14:55:40 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,19 +11,24 @@ class Artist
     @@all
   end
 
-  def songs
-    Song.find_by_artist(self)
+  def add_song(song)
+    @songs << song
   end
 
-  def self.find_or_create_by_name(name)
-    self.find(name) ? self.find(name) : self.create(name)
+  def self.all
+    @@all
   end
 
-  def self.find(name)
-    self.all.find {|artist| artist.name == name }
+  def save
+    @@all << self
   end
+def self.find_or_create_by_name(name)
+self.find(name)? self.find(name): self.crreate(name)
+end
 
-
+def self.find(name)
+self.all.find {|artist| artist.name == name}
+end
   def self.create(name)
     self.new(name).tap {|artist| artist.save}
   end
@@ -32,10 +37,10 @@ class Artist
     @@all << self
   end
 
-  # def print_songs
-  # 		@songs.each do |song|
-  # 			puts song.name
-  # 		end
-  # 	end
+  def print_songs
+  		@songs.each do |song|
+  			puts song.name
+  		end
+  	end
 
 end
